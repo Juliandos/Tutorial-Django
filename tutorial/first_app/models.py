295187d1e642b41948beeedf02100bdb31cd53ce
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-10-22 22:22:42 -0500

    feat: Learn how to rerelate tables

--- tutorial/first_app/models.py
+++ tutorial/first_app/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     cuisine = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     year = models.IntegerField()
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None)
+
 
     def __str__(self):
         return self.name
