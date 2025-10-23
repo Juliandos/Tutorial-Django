9e657f1ee540625725f6eaac168a86c375286b1a
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-10-23 10:10:17 -0500

    feat: admin is working

--- tutorial/first_app/admin.py
+++ tutorial/first_app/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
-from .models import Menu, MenuCategory
+from .models import Category, MenuItem
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'menu_items_count', 'created_at']
+    search_fields = ['name', 'description']
+    list_per_page = 20
+    
+    def menu_items_count(self, obj):
+        return obj.menu_items.count()
+    menu_items_count.short_description = 'Nº de Platos'
 
-admin.site.register(Menu)
-admin.site.register(MenuCategory)
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'is_available', 'is_vegetarian', 'spicy_level', 'preparation_time']
+    list_filter = ['category', 'is_available', 'is_vegetarian', 'spicy_level']
+    search_fields = ['name', 'description']
+    list_editable = ['price', 'is_available']
+    list_per_page = 20
+    
+    fieldsets = (
+        ('Información Básica', {
+            'fields': ('name', 'category', 'description')
+        }),
+        ('Precio y Disponibilidad', {
+            'fields': ('price
